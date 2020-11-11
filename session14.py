8ef7665d42a2052727cabbf418fe5587e5204d14
Pepe García <pepe@pepegar.com>
2020-11-11 01:56:47 +0100

    first commit

---
+++ session14.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#%% Exercise 1
+
+# import the utitities.py file, let's see what happens
+
+#%% Exercise 2
+
+#1. use the module located in code/utils/data.py
+#2. print the second element from the list
---
+++ utilities.py
@@ -0,0 +1,11 @@
+import time
+
+i = 0
+
+while True:
+
+    time.sleep(0.5)
+
+    print(i)
+
+    i += 1
